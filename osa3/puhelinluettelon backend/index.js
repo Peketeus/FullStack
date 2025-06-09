9852943fdbf82261cd1b65407e683f8f6c05efe1
Peketeus <p3keteus@gmail.com>
2025-06-09 19:42:13 +0300

    Tehtävät 3.5 ja 3.6

--- osa3/puhelinluettelon backend/index.js
+++ osa3/puhelinluettelon backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: "1",
@@ -61,6 +63,37 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  const personExists = persons.find(person => person.name === body.name)
+
+  if (personExists) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const newId = Math.floor(Math.random() * 100001)
+
+  const person = {
+    id: String(newId),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
