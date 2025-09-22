796eca5d4cbdabc277e54e068cb8081c83c7f15b
Peketeus <p3keteus@gmail.com>
2025-09-22 14:34:19 +0300

    Käyttäjien lisääminen ja - hallinta

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -4,10 +4,12 @@ const blogsRouter = require('express').Router()
 const { request } = require('express')
 const Blog = require('../models/blog')
 const { response } = require('../app')
+const User = require('../models/user')
 
 // hakee blogit tietokannasta
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -25,11 +27,18 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user._id
   })
 
   // jos 'title' tai 'url' puuttuu, hylätään tallentaminen ja palautetaan error 400 (Bad Request)
@@ -38,6 +47,9 @@ blogsRouter.post('/', async (request, response) => {
   }
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
