2ff2b09e2c012baca83f6536be1058447cdbdd5f
Peketeus <p3keteus@gmail.com>
2025-09-26 20:23:50 +0300

    Lisää tehtävä 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+import '../index.css'
+
+const Blog = ({ blog }) => {
+  const [moreDetails, setMoreDetails] = useState(false)
+
+  return (
+    <div className="blog">
+      {blog.title} | {blog.author}{' '}
+      <button onClick={() => setMoreDetails(!moreDetails)}>
+        {moreDetails ? 'hide' : 'view'}
+      </button>
+
+      {moreDetails && (
+        <div>
+          <p>Url: {blog.url}</p>
+          <p>Likes: {blog.likes}</p>
+          <p>Added by user: {blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  ) 
+}
 
 export default Blog
